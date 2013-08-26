fc5d4095f8217075195cb84fb1a0743e111157b3
Luke <doclukemckee@gmail.com>
2013-08-27 05:10:33 +1200

    views improvement. 404 implementation

--- polls/views.py
+++ polls/views.py
@@ -1,14 +1,20 @@
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.template import RequestContext, loader
+from django.shortcuts import render, get_object_or_404
 from polls.models import Poll
 
 def index(request):
     latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
-    output = ', '.join([p.question for p in latest_poll_list])
-    return HttpResponse(output)
+    template = loader.get_template('polls/index.html')
+    context = RequestContext(request, {
+            'latest_poll_list': latest_poll_list,
+            })
+    return HttpResponse(template.render(context))
 
 def detail(request, poll_id):
-    return HttpResponse("You're looing at poll {0}".format(poll_id))
+    poll = get_object_or_404(Poll, pk=poll_id)
+    return render(request,'polls/detail.html',{'poll':poll})
 
 def results(request, poll_id):
     return HttpResponse("You're looking at the result sof poll {0}".format(poll_id))
