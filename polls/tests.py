94702cbd606ab87f73803f522260581bfc2642cf
Luke <doclukemckee@gmail.com>
2013-08-27 16:59:37 +1200

    Unittest for published_recently

--- polls/tests.py
+++ polls/tests.py
@@ -6,7 +6,11 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from django.utils import timezone
 
+import datetime
+
+from polls.models import Poll
 
 class SimpleTest(TestCase):
     def test_basic_addition(self):
@@ -14,3 +18,13 @@ class SimpleTest(TestCase):
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+        
+class PollMethodTests(TestCase):
+    
+    def test_was_published_recently_with_future_poll(self):
+        """
+        was_published_recently() should return False for polls whose
+        pub_date is in the future
+        """
+        future_poll = Poll(pub_date=timezone.now() + datetime.timedelta(days=30))
+        self.assertEqual(future_poll.was_published_recently(), False)
